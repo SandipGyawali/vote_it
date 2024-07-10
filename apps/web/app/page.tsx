f3213741df624a7bb44b14de67f9bab2af579052
Sandip Gyawali <sandipgyawali10@gmail.com>
2024-07-10 11:47:29 +0545

    websocket client integratoin

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+import { socket } from "@/socket";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [isConnected, setIsConnected] = useState<boolean>(false);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("SOcket io connection established");
+    });
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello, Google docs mini clone.</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          socket.emit("hello");
+        }}
+      >
+        <button type="submit">Click for hello event</button>
+      </form>
     </main>
   );
 }
